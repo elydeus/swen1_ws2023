95397df12ff2fd0c5fdb5b5a9fa4b14aec4f6c61
felderer <if23b058@technikum-wien.at>
2024-01-04 16:34:24 +0100

    added AbstractController Logic and multiple HTTP Status Codes

--- src/main/java/at/technikum/apps/mtcg/controller/AbstractController.java
+++ src/main/java/at/technikum/apps/mtcg/controller/AbstractController.java
@@ -20,6 +20,93 @@ public abstract class AbstractController {
         return response;
     }
 
-    // THOUGHT: more functionality e.g. ok(), json(), etc
+    protected Response ok(HttpStatus httpStatus){
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"ok\": \""+ HttpStatus.OK + "\"}");
+        return response;
+    }
 
+    protected Response notAllowed(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Not Allowed\": \""+ HttpStatus.NOT_ALLOWED + "\"}");
+
+        return response;
+    }
+
+    protected Response created(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Created\": \""+ HttpStatus.CREATED + "\"}");
+
+        return response;
+    }
+
+    protected Response badRequest(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Bad Request\": \""+ HttpStatus.BAD_REQUEST + "\"}");
+
+        return response;
+    }
+
+    protected Response internalServerError(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Bad Request\": \"" + HttpStatus.INTERNAL_SERVER_ERROR + "\"}");
+
+        return response;
+    }
+
+    protected Response accepted(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Accepted\": \""+ HttpStatus.ACCEPTED + "\"}");
+
+        return response;
+    }
+
+    protected Response unauthorized(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Accepted\": \""+ HttpStatus.UNAUTHORIZED + "\"}");
+
+        return response;
+    }
+
+    protected Response notFound(HttpStatus httpStatus) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody("{ \"Not Found\": \""+ HttpStatus.NOT_FOUND + "\"}");
+
+        return response;
+    }
+
+    protected Response json(HttpStatus httpStatus, String jsonResponse) {
+        Response response = new Response();
+        response.setStatus(httpStatus);
+        response.setContentType(ContentType.APPLICATION_JSON);
+        response.setBody(jsonResponse);
+
+        return response;
+    }
+    protected String extractUser(String header){
+        String[] sections = header.split(" ");
+        if (sections.length == 2 && sections[0].equals("Bearer")){
+            String[] subSections = sections[1].split("-");
+            if (subSections.length == 2){
+                return subSections[0];
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/at/technikum/server/http/HttpStatus.java
+++ src/main/java/at/technikum/server/http/HttpStatus.java
@@ -1,11 +1,18 @@
 package at.technikum.server.http;
 
-// THOUGHT: Add new relevant status (https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
 public enum HttpStatus {
-    OK(200, "OK"),
-    BAD_REQUEST(400, "Bad Request"),
-    NOT_FOUND(404, "Not Found");
-
+    OK (200, "OK"),
+    BAD_REQUEST (400, "Bad Request"),
+    NOT_FOUND (404, "Not Found"),
+    CREATED (201, "Created"),
+    ACCEPTED (202, "Accepted"),
+    UNAUTHORIZED (401, "Unauthorized"),
+    FORBIDDEN (403, "Forbidden"),
+    NOT_ALLOWED (405, "Not Allowed"),
+    CONFLICT (409, "Conflict"),
+    INTERNAL_SERVER_ERROR (500, "Internal Server Error"),
+    INSUFFICIENT_COINS (402, "Payment Required - Insufficient Coins"),
+    DECK_INCOMPLETE (422, "Unprocessable Content - Incomplete Deck");
     private final int code;
     private final String message;
 
